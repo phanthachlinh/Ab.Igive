7971a7d563716eee9eab450265d445f33813c570
thachlinhphan <jakuzax@gmail.com>
2019-11-08 21:25:08 +0100

    switch to desktop

---
+++ src/app.js
@@ -0,0 +1,13 @@
+/*let body = document.getElementsByTagName('body')[0];
+let languagesContainer =document.querySelector('nav .dropdown');
+let langChooser = document.querySelector('nav .top-bar-section .has-dropdown>a');
+//move lang container
+languagesContainer.classList.add('lang-container');
+body.insertAdjacentHTML('afterBegin',languagesContainer.outerHTML);
+languagesContainer.remove();
+//add lang event listener
+langChooser.addEventListener('click',(ev)=>{
+  document.querySelector('.lang-container').class
+})
+console.log('dsf')
+*/
